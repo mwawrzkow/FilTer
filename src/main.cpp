513e08d4178edcb33d131f009032fac789f3ba40
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2019-10-10 13:52:11 +0200

    Add: File Dir https://github.com/mwawrzkow/colo/blob/cf12687089268a174a256f48f04196631bed1b02/src/FileIOController.h#L11 https://github.com/mwawrzkow/colo/blob/cf12687089268a174a256f48f04196631bed1b02/src/FileIOController.cpp#L10

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include "Handler/Handler.hpp"
 #include  <iostream>
 
-using namespace std;
 
+//https://github.com/mwawrzkow/colo/blob/master/src/FileIOController.h#L11
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+const char slash = 0x5C;
+
+#else
+const char slash = 0x2F;
+
+#endif
+// https://github.com/mwawrzkow/colo/blob/master/src/FileIOController.cpp#L10
+namespace FileIO {
+
+int lastslash(char findchar, std::string ourstring) {
+	std::size_t temp;
+	temp = ourstring.find_last_of(findchar);
+	return temp;
+}
+void getexepath(const std::string exeLocation, std::string &filelocation) {
+	filelocation = exeLocation;
+	std::size_t whereami = lastslash(slash, filelocation);
+	filelocation.erase(filelocation.begin() + (whereami + 1),
+			filelocation.end());
+}
+
+} /* namespace FileIO */
 
 
 int main() {
